679651ff3bf0cac7aebefb71718aef29d35e1bf2
neritoo <ezegavilan95@gmail.com>
2020-11-24 18:48:28 -0300

    commit test

--- src/main/java/com/deofis/tiendaapirest/config/SecurityConfig.java
+++ src/main/java/com/deofis/tiendaapirest/config/SecurityConfig.java
@@ -39,7 +39,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
     private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
-
     private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
 
     private final JwtFiltroAutenticacion jwtFiltroAutenticacion;
