dc9db7ed22246a32303859d1a362a842663e99ed
neritoo <ezegavilan95@gmail.com>
2020-11-25 20:16:03 -0300

    Diseño de servicios, entidades, y clases utils. (Imagen).

--- src/main/java/com/deofis/tiendaapirest/productos/services/images/ImageService.java
+++ src/main/java/com/deofis/tiendaapirest/productos/services/images/ImageService.java
@@ -5,11 +5,32 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+/**
+ * Este servicio se encarga de subir las imágenes utilizadas por Objetos que
+ * los necesiten ({@link com.deofis.tiendaapirest.productos.domain.Producto}
+ * y {@link com.deofis.tiendaapirest.productos.domain.Sku}).
+ */
+
 public interface ImageService {
 
+    /**
+     * Método que se encarga de subir la imágen.
+     * @param archivo MultipartFile archivo a subir.
+     * @return {@link Imagen} con los datos cargados de la subida.
+     */
     Imagen subirImagen(MultipartFile archivo);
 
+    /**
+     * Devuelve bytes asociados a una imágen guardada.
+     * @param path String path del archivo imagen a descargar.
+     * @return byte[] bytes del archivo imagen.
+     * @throws IOException por los archivos.
+     */
     byte[] descargarImagen(String path) throws IOException;
 
+    /**
+     * Elimina un archivo de imagen a través de su path.
+     * @param path String path del archivo imagen a eliminar.
+     */
     void eliminarImagen(String path);
 }
