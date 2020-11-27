8f7c8bf8d8ef03ae670e8852393f65dbed9b5599
neritoo <ezegavilan95@gmail.com>
2020-11-27 18:39:21 -0300

    HealthCheckController para AWS ElasticBeanstalk.

---
+++ src/main/java/com/deofis/tiendaapirest/aws/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.deofis.tiendaapirest.aws;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+
+    @GetMapping("")
+    public ResponseEntity healthCheck() {
+        return ResponseEntity.ok().body("OK");
+    }
+}
