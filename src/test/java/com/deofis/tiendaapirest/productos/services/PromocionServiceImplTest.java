20d014916b26f29828eff2ea3404bb201ed54fb1
neritoo <ezegavilan95@gmail.com>
2020-12-03 10:47:43 -0300

    HOTFIX: Precio y Disponibilidad @NotNull.

--- src/test/java/com/deofis/tiendaapirest/productos/services/PromocionServiceImplTest.java
+++ src/test/java/com/deofis/tiendaapirest/productos/services/PromocionServiceImplTest.java
@@ -8,13 +8,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
 @SpringBootTest
-@ActiveProfiles("dev")
 @Slf4j
 class PromocionServiceImplTest {
 
