1a220148be3021237779cd2cc20da2a278b0e5c8
neritoo <ezegavilan95@gmail.com>
2020-12-15 13:44:37 -0300

    Active profile: dev. en los test.

--- src/test/java/com/deofis/tiendaapirest/productos/services/SkuServiceImplTest.java
+++ src/test/java/com/deofis/tiendaapirest/productos/services/SkuServiceImplTest.java
@@ -9,12 +9,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Map;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 @Slf4j
 class SkuServiceImplTest {
 
